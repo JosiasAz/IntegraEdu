35aa3ca2cd348f019e51a578491437211e50fd7b
JosiasAz <jjosias00@gmail.com>
2026-02-08 15:55:27 -0400

    feat: renomear projeto para IntegraEdu e atualizar componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './shared/login-component/login-component';
+import { Python } from './pages/python/python';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, Python],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
